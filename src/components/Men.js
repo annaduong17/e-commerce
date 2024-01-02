9b57be66e39733152b97771fed02078fe4ede835
Anna Duong <aduong9417@gmail.com>
2024-01-01 17:48:10 -0800

    render men's shoes in Men component

--- src/components/Men.js
+++ src/components/Men.js
@@ -1,6 +1,27 @@
+import Data from '../data/data.json';
+import Product from './Product';
+
 function Men() {
+  const menShoes = Data.filter(item => {
+    return item.gender === 'men';
+  })
+
+  const renderedItems = menShoes.map(item => {
+    return(
+      <Product 
+        key={item.id}
+        imgUrl={`/images/products/${item.imageFile}`}
+        name={item.name}
+        description={item.description}
+        price={item.price}
+        discount={50}
+      />
+    )
+  })
+
+  console.log(renderedItems);
   return(
-    <div>Men's shoes</div>
+    <div>{renderedItems}</div>
   )
 }
 
