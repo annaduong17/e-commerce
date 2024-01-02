a48b008974bc04151ac9c8076e9bce080ddd327a
Anna Duong <aduong9417@gmail.com>
2024-01-02 11:32:20 -0800

    add .00 to prices that are whole numbers

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -3,6 +3,9 @@ import Button from './Button';
 
 function ProductDetails({ id, name, description, price, discount, imgUrl, handleAddProduct }) {
   const discountedPrice = price * (discount/100);
+  const isWholeNumber = (number) => {
+    return number % 1 === 0;
+  }
 
   return(
     <div className='details'>
@@ -10,10 +13,10 @@ function ProductDetails({ id, name, description, price, discount, imgUrl, handle
       <h1 className='name'>{name}</h1>
       <p className='description'>{description}</p>
       <div className='price-discount-container'>
-        <span className='discounted-price'>${discountedPrice}.00</span>
+        <span className='discounted-price'>${discountedPrice}{isWholeNumber(discountedPrice) && '.00'}</span>
         <span className='discount'>{discount}%</span>
       </div>
-      <p className='price'>${price}.00</p>
+      <p className='price'>${price}{isWholeNumber(price) && '.00'}</p>
       <div className='add-quantity-container'>
         <ProductQuantity id={id} />
         <Button id={id} onClick={() => handleAddProduct(id, name, price, imgUrl)} img={'/images/icons/icon-cart-white.svg'}className="orange-btn">Add to cart</Button>
