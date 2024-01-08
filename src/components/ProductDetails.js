fdc598ba2bf6ba9dfe43a7582d9744216371ca0d
Anna Duong <aduong9417@gmail.com>
2024-01-07 17:27:34 -0800

    add functions countDecimals and finalPrice to add appropriate number of zeros to price

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -2,9 +2,28 @@ import ProductQuantity from './ProductQuantity';
 import Button from './Button';
 
 function ProductDetails({ id, name, description, price, discount, imgUrls, handleAddProduct }) {
+
   const discountedPrice = price * (discount/100);
+  
   const isWholeNumber = (number) => {
     return number % 1 === 0;
+  }
+
+  const countDecimals = (number) => {
+    if (Math.floor(number) === number) return 0;
+    return number.toString().split(".")[1].length || 0;
+  }
+
+  const finalPrice = (price) => {
+    if (isWholeNumber(price)) {
+      return `${price}.00`;
+    } else {
+      if (countDecimals(price) === 1) {
+        return `${price}0`;
+      } else if (countDecimals(price) === 2) {
+        return `${price}`;
+      }
+    }
   }
 
   return(
@@ -13,10 +32,10 @@ function ProductDetails({ id, name, description, price, discount, imgUrls, handl
       <h1 className='name'>{name}</h1>
       <p className='description'>{description}</p>
       <div className='price-discount-container'>
-        <span className='discounted-price'>${discountedPrice}{isWholeNumber(discountedPrice) && '.00'}</span>
+        <span className='discounted-price'>{finalPrice(discountedPrice)}</span>
         <span className='discount'>{discount}%</span>
       </div>
-      <p className='price'>${price}{isWholeNumber(price) && '.00'}</p>
+      <p className='price'>{finalPrice(price)}</p>
       <div className='add-quantity-container'>
         <ProductQuantity id={id} />
         <Button id={id} onClick={() => handleAddProduct(id, name, price, imgUrls)} img={'/images/icons/icon-cart-white.svg'}className="orange-btn">Add to cart</Button>
